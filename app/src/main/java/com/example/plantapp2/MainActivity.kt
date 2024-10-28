7b95182ea2ce38be2335bcd58204423cfa1619b7
benjamin <s235097@dtu.dk>
2024-10-28 16:25:00 +0100

    menubar er klar

--- app/src/main/java/com/example/plantapp2/MainActivity.kt
+++ app/src/main/java/com/example/plantapp2/MainActivity.kt
@@ -19,14 +19,28 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -37,7 +51,12 @@ import androidx.compose.ui.unit.dp
 import com.example.plantapp2.Data.Datasource
 import com.example.plantapp2.Model.affirmation
 import com.example.plantapp2.ui.theme.Plantapp2Theme
+data class BottomNavItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
 
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,19 +64,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Plantapp2Theme {
+                val item = listOf(
+                    BottomNavItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home
+                    ),
+                    BottomNavItem(
+                        title = "Search",
+                        selectedIcon = Icons.Filled.Search,
+                        unselectedIcon = Icons.Outlined.Search
+                    ),
+                    BottomNavItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings
+                    ),
+
+                )
+                var selectedIremIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
                     Scaffold(bottomBar ={
                             NavigationBar {
+                                item.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedIremIndex == index,
+                                        onClick = {
+                                            selectedIremIndex = index
+                                            //navController.navigate(item.title)
+                                        },
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (selectedIremIndex == index) item.selectedIcon else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
 
+                                        }
+                                    )
+                                }
                             }
                     }
                     ) {
 
                     }
-                    AffirmationsApp()
+                    //AffirmationsApp()
 
                 }
 
