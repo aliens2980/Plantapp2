2dd3158ac2e21c17b574dcb5ae39bfa6789ab9c3
JalokinBo <Nikolajbo@gmail.com>
2024-10-31 18:09:25 +0100

    Filter knap tilføjet.

--- app/src/main/java/com/example/plantapp2/navigation/components/SearchBar.kt
+++ app/src/main/java/com/example/plantapp2/navigation/components/SearchBar.kt
@@ -10,9 +10,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.ui.res.painterResource
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -51,82 +53,100 @@ fun MainSearch() {
     val items = remember {
         mutableStateListOf(
             Plant("Kartofler", "Grøntsag", "Brun", 10.0, 20.0, true),
-            Plant("Jordbær", "Nød", "Rød",25.0,10.0, false),
+            Plant("Jordbær", "Nød", "Rød", 25.0, 10.0, false),
             Plant("Brændnæller", "Urt", "Grøn", 30.0, 30.0, true),
             Plant("Gulerødder", "Grøntsag", "Orange", 10.0, 40.0, false),
-            Plant("Oliven", "Frugt", "Grøn", 100.0,40.0, true),
-            Plant("Tomato", "Grøntsag", "Rød",80.0,50.0, true),
+            Plant("Oliven", "Frugt", "Grøn", 100.0, 40.0, true),
+            Plant("Tomato", "Grøntsag", "Rød", 80.0, 50.0, true),
             Plant("Agurk", "Grøntsag", "Grøn", 25.0, 30.0, false)
         )
     }
 
-    Scaffold {
-        SearchBar(
-            modifier = Modifier.fillMaxWidth(),
-            query = text,
-            onQueryChange = {text = it},
-            onSearch = {
-                items.add(Plant(text, "NewType", "Color", 0.0, 0.0, true))
-                active = false
-            },
-            active = active,
-            onActiveChange = {active = it},
-            placeholder = { Text(text = "Search") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon") },
-            trailingIcon = {
-                if (active) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = "Close Icon",
-                        modifier = Modifier.clickable {
-                            if (text.isNotEmpty()) {
-                                text = ""
-                            } else {
-                                active = false
-                            }
-
-                        }
-                    )
-                }
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = Color.DarkGray,
+                contentColor = Color.White,
+            ) {
+                Icon(
+                    Icons.Default.MoreVert,
+                    contentDescription = "Filter Icon"
+                )
             }
+        }
         ) {
-            items.forEach { plant ->
-                Row(modifier = Modifier.padding(all = 16.dp)) {
+            Column {
+            SearchBar(
+                modifier = Modifier.fillMaxWidth(),
+                query = text,
+                onQueryChange = { text = it },
+                onSearch = {
+                    items.add(Plant(text, "NewType", "Color", 0.0, 0.0, true))
+                    active = false
+                },
+                active = active,
+                onActiveChange = { active = it },
+                placeholder = { Text(text = "Search") },
+                leadingIcon = {
                     Icon(
-                        painter = painterResource(id = R.drawable.psychiatry_24px),
-                        contentDescription = "plant icon",
-                        tint = Color.Green,
-                        modifier = Modifier.padding(end = 16.dp)
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon"
                     )
-                    Column {
-                        Row {
-                            Text(
-                                text = plant.name
-                            )
-                            if(plant.favorite) {
-                                Icon(
-                                imageVector = Icons.Default.Favorite,
-                                contentDescription = "Favorite Icon",
-                                tint = Color.Red,
-                                modifier = Modifier.padding(start = 8.dp)
-                                )
-                            } else {
-                                Icon(
-                                    imageVector = Icons.Default.FavoriteBorder,
-                                    contentDescription = "Not Favorite Icon",
-                                    tint = Color.Red,
-                                    modifier = Modifier.padding(start = 8.dp)
-                                )
+                },
+                trailingIcon = {
+                    if (active) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Close Icon",
+                            modifier = Modifier.clickable {
+                                if (text.isNotEmpty()) {
+                                    text = ""
+                                } else {
+                                    active = false
+                                }
                             }
+                        )
+                    }
+                }
+            ) {
+                items.forEach { plant ->
+                    Row(modifier = Modifier.padding(all = 16.dp)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.psychiatry_24px),
+                            contentDescription = "plant icon",
+                            tint = Color.Green,
+                            modifier = Modifier.padding(end = 16.dp)
+                        )
+                        Column {
+                            Row {
+                                Text(
+                                    text = plant.name
+                                )
+                                if (plant.favorite) {
+                                    Icon(
+                                        imageVector = Icons.Default.Favorite,
+                                        contentDescription = "Favorite Icon",
+                                        tint = Color.Red,
+                                        modifier = Modifier.padding(start = 8.dp)
+                                    )
+                                } else {
+                                    Icon(
+                                        imageVector = Icons.Default.FavoriteBorder,
+                                        contentDescription = "Not Favorite Icon",
+                                        tint = Color.Red,
+                                        modifier = Modifier.padding(start = 8.dp)
+                                    )
+                                }
+                            }
+                            Text(text = plant.type)
                         }
-                        Text(text = plant.type)
                     }
                 }
             }
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun SearchBarPreview (){
