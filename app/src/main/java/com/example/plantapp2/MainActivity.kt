b58d615867cc4295f6fed1881b676f938bc74675
benjamin <s235097@dtu.dk>
2024-10-31 14:29:36 +0100

    merge fra menubar virker

--- app/src/main/java/com/example/plantapp2/MainActivity.kt
+++ app/src/main/java/com/example/plantapp2/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
