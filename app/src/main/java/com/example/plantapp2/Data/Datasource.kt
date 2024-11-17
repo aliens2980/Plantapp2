f9e5ef16769e12d2e65034d365174c62cd6f0da7
Louise Lund <s153976@dtu.dk>
2024-11-17 12:27:27 +0100

    Hjælp fra Lucas

--- app/src/main/java/com/example/plantapp2/Data/Datasource.kt
+++ app/src/main/java/com/example/plantapp2/Data/Datasource.kt
@@ -1,22 +1,22 @@
 package com.example.plantapp2.Data
 
-import com.example.plantapp2.Models.affirmation
+import com.example.plantapp2.Models.Affirmation
 import com.example.plantapp2.R
 
 
 class Datasource() {
-    fun loadAffirmations(): List<affirmation> {
-        return listOf<affirmation>(
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.plantapp, R.drawable.horde_background),
-            affirmation(R.string.plantapp, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background),
-            affirmation(R.string.hello_world, R.drawable.horde_background)
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.plantapp, R.drawable.horde_background),
+            Affirmation(R.string.plantapp, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background),
+            Affirmation(R.string.hello_world, R.drawable.horde_background)
         )
     }
 }
