fcde6e1045c17c6bd3a89bc4ec25a22ea21bff10
Petrine <s235064@dtu.dk>
2024-11-21 18:57:31 +0100

    semi makeover of main bed page

--- app/src/main/java/com/example/plantapp2/ui/theme/Bed.kt
+++ app/src/main/java/com/example/plantapp2/ui/theme/Bed.kt
@@ -1,40 +1,72 @@
 package com.example.plantapp2.ui.theme
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.plantapp2.R
 
 
 /**
- * @author s235109
- * @param PlantBed
- * @return A grid in which each space is clickable.
+ *
  */
 
+
+//Controls the entire setup of the page! Only alter the placements of things in this one!
 @Composable
 fun CenteredBed(length: Int, width: Int, gridSize: Int = 60) {
     Box(
-        modifier = Modifier.fillMaxSize(), // Outer Box to take up full screen
-        contentAlignment = Alignment.Center // Align content in the center
-    ) {
-        Bed(length = length, width = width, gridSize = gridSize)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFDAD7CD))
+            .verticalScroll(rememberScrollState())
+    )
+    {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally // Center the grid horizontally
+        ) {
+            Spacer(modifier = Modifier.height(90.dp))
+
+            Bed(length = length, width = width, gridSize = gridSize)
+        }
+
     }
+
 }
+
+
 
 @Composable
 fun Bed(length: Int, width: Int, gridSize: Int = 60, viewModel: GridViewModel = viewModel()) {
@@ -48,7 +80,8 @@ fun Bed(length: Int, width: Int, gridSize: Int = 60, viewModel: GridViewModel = 
     Box(
         modifier = Modifier
             .size(width.dp, length.dp)
-            .background(color = brown)
+            .padding(6.dp)  //adds the space between the bed and the outer walls of the page
+            //.background(color = brown)
             .testTag("Grid")
     )
     {
@@ -60,12 +93,18 @@ fun Bed(length: Int, width: Int, gridSize: Int = 60, viewModel: GridViewModel = 
     }
 }
 
+
+
+
 @Composable
 fun DrawGridLines(gridSize: Int) {
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawGridLines(gridSize)
     }
 }
+
+
+
 
 @Composable
 fun DrawGridCells(numRows: Int, numColumns: Int, gridSize: Int, viewModel: GridViewModel) {
@@ -76,7 +115,12 @@ fun DrawGridCells(numRows: Int, numColumns: Int, gridSize: Int, viewModel: GridV
                     // Get the clicked state from the ViewModel
                     val isSelected = viewModel.gridState[rowIndex][columnIndex]
 
-                    GridCell(row = rowIndex, column = columnIndex, gridSize = gridSize, isSelected = isSelected) {
+                    GridCell(
+                        row = rowIndex,
+                        column = columnIndex,
+                        gridSize = gridSize,
+                        imageId =  R.drawable.dirt,  //remove if not working
+                        isSelected = isSelected) {
                         // Toggle the state in the ViewModel when the cell is clicked
                         viewModel.toggleCell(rowIndex, columnIndex)
                     }
@@ -86,15 +130,41 @@ fun DrawGridCells(numRows: Int, numColumns: Int, gridSize: Int, viewModel: GridV
     }
 }
 
+
+
+
 @Composable
-fun GridCell(row: Int, column: Int, gridSize: Int, isSelected: Boolean, onClick: () -> Unit) {
+fun GridCell(
+    row: Int,
+    column: Int,
+    gridSize: Int,
+    imageId: Int,
+    isSelected: Boolean,
+    onClick: () -> Unit
+
+) {
+
     Box(
         modifier = Modifier
             .size(gridSize.dp)
             .background(if (isSelected) Color.Green else Color.Transparent)
             .clickable { onClick() }
             .testTag("Cell-$row-$column") // Add a unique tag for each cell
-    )
+    ) {
+        //image background for each cell (not ideal solution right now)
+        Image (
+            painter = painterResource(id = R.drawable.dirt),
+            contentDescription = "Grid cell image",
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .padding(0.dp)
+                .size(gridSize.dp-4.dp)
+                .fillMaxSize()
+                //.clipToBounds()
+        )
+
+
+    }
 }
 
 fun DrawScope.drawGridLines(gridSize: Int) {
