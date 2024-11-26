756dcc32c04cabac8b3bdb2d049b6a58cf1d3d70
Louise Lund <s153976@dtu.dk>
2024-11-26 15:57:37 +0100

    Remove Lorem Ipsen text

--- app/src/main/java/com/example/plantapp2/ui/theme/PlantPage.kt
+++ app/src/main/java/com/example/plantapp2/ui/theme/PlantPage.kt
@@ -76,16 +76,6 @@ fun PlantInfoPage(navController: NavController, modifier: Modifier = Modifier, g
     val water = plant?.water
     val sun = plant?.sun
     val gradeText = plant?.gradeText
-    val lorem = "Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.\n" +
-            "\n" +
-            "Hvorfor bruger vi det?\n" +
-            "Det er en kendsgerning, at man bliver distraheret af læsbart indhold på en side, når man betragter dens layout. Meningen med at bruge Lorem Ipsum er, at teksten indeholder mere eller mindre almindelig tekstopbygning i modsætning til \"Tekst her - og mere tekst her\", mens det samtidigt ligner almindelig tekst. Mange layoutprogrammer og webdesignere bruger Lorem Ipsum som fyldtekst. En søgning på Lorem Ipsum afslører mange websider, som stadig er på udviklingsstadiet. Der har været et utal af variationer, som er opstået enten på grund af fejl og andre gange med vilje (som blandt andet et resultat af humor).\n" +
-            "\n" +
-            "\n" +
-            "Hvor kommer det fra?\n" +
-            "I modsætning til hvad mange tror, er Lorem Ipsum ikke bare tilfældig tekst. Det stammer fra et stykke litteratur på latin fra år 45 f.kr., hvilket gør teksten over 2000 år gammel. Richard McClintock, professor i latin fra Hampden-Sydney universitet i Virginia, undersøgte et af de mindst kendte ord \"consectetur\" fra en del af Lorem Ipsum, og fandt frem til dets oprindelse ved at studere brugen gennem klassisk litteratur. Lorem Ipsum stammer fra afsnittene 1.10.32 og 1.10.33 fra \"de Finibus Bonorum et Malorum\" (Det gode og ondes ekstremer), som er skrevet af Cicero i år 45 f.kr. Bogen, som var meget populær i renæssancen, er en afhandling om etik. Den første linie af Lorem Ipsum \"Lorem ipsum dolor sit amet...\" kommer fra en linje i afsnit 1.10.32.\n" +
-            "\n" +
-            "Standardafsnittet af Lorem Ipsum, som er brugt siden 1500-tallet, er gengivet nedenfor for de, der er interesserede. Afsnittene 1.10.32 og 1.10.33 fra \"de Finibus Bonorum et Malorum\" af Cicero er også gengivet i deres nøjagtige udgave i selskab med den engelske udgave fra oversættelsen af H. Rackham fra 1914."
 
     LaunchedEffect(getPlantName) {
         if (getPlantName != null) {
@@ -236,18 +226,8 @@ fun PlantInfoPage(navController: NavController, modifier: Modifier = Modifier, g
                         }
                     }
 
-                    lorem.let {Row(verticalAlignment = Alignment.CenterVertically) {
-                        SunImage(modifier = Modifier.size(48.dp))
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Lorem(information = it)
-                    }
-                    }}
-
-
-
-
                 }
-
+            }
                 Spacer(modifier = Modifier.height(32.dp))
             }
         }
@@ -514,34 +494,6 @@ fun GradeImage(modifier: Modifier) {
         )
     }
 
-}
-
-@Composable
-fun Lorem(information: String, modifier: Modifier = Modifier) {
-    BoxWithConstraints(
-        modifier = modifier
-            //.offset(x = 65.dp, y = 440.dp)
-            //.background(Color.White)
-            //.border(BorderStroke(1.dp, Color.Black))
-            .padding(8.dp)
-    ) {
-        val maxWidth = 200.dp
-        val maxHeight = maxHeight
-
-        Text(
-            text = "Is graded to be: $information",
-            modifier = Modifier
-                .widthIn(max = maxWidth)
-                .heightIn(max = maxHeight)
-                .padding(4.dp),
-            style = TextStyle(   //to edit and customize the text inside
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Light,
-                fontFamily = FontFamily.Default,
-                color = Color.DarkGray
-            )
-        )
-    }
 }
 
 //Grade text box
